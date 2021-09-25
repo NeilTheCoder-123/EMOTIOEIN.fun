08f998098eaaece870a437647bbb6484b7e71bfd
NeilTheCoder-123 <78020134+NeilTheCoder-123@users.noreply.github.com>
2021-09-25 18:23:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function capture(){
 }
 console.log(ml5.version);
 
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/m0ZyZFP-7/model.json", modelLoaded);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/CodJGRiEF/model.json", modelLoaded);
 
 function modelLoaded(){
     console.log("modelLoaded!!!!!");
@@ -30,4 +30,40 @@ function speak(){
     speak_data_2 = "The second prediction is"+prediction_2;
     var utterThis = new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
     synth.speak(utterThis);
+}
+
+function check(){
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error){
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if (results[0].label == "happy"){
+            document.getElementById("update_emoji").innerHTML = "😊";
+        }
+        if (results[0].label == "angry"){
+            document.getElementById("update_emoji").innerHTML = "😠";
+        }
+        if (results[0].label == "sad"){
+            document.getElementById("update_emoji").innerHTML = "😌";
+        }
+        if (results[1].label == "happy"){
+            document.getElementById("update_emoji2").innerHTML = "😊";
+        }
+        if (results[1].label == "angry"){
+            document.getElementById("update_emoji2").innerHTML = "😠";
+        }
+        if (results[1].label == "sad"){
+            document.getElementById("update_emoji2").innerHTML = "😌";
+        }
+    }
 }
